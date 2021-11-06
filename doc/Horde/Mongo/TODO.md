62166f6a86ffd84f4acc9be6237992d0ca7df226
Ralf Lang <lang@b1-systems.de>
2021-11-06 07:46:47 +0000

    Released Mongo-2.0.0alpha2

---
+++ doc/Horde/Mongo/TODO.md
@@ -0,0 +1,7 @@
+# PHP 7/8 inspired todos
+
+Horde_Mongo's original intent was to be both compatible with the "mongo" extension and the "mongodb" extension.
+The older "mongo" extension is not available for PHP 7/8. Thus we will always use the compatibility layer.
+
+- Use ext/mongodb extension natively
+- remove dependency on external wrapper library
---
+++ test/NullClientTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright 2016-2021 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Cache
+ */
+
+namespace Horde\Mongo\Test;
+
+use Horde\Mongo\Client;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * A basic testcase for CLI
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL 2.1
+ * @package  Cache
+ */
+class NullClientTest extends TestCase
+{
+    public function setUp(): void
+    {
+        $this->nullClient = new Client();
+    }
+
+    public function testGetsConstructed()
+    {
+        $this->assertInstanceOf(Client::class, $this->nullClient);
+    }
+}
